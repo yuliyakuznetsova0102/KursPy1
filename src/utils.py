6d73e045673d4ea2a99244795b1c49a01b14ff7c
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-10-07 19:44:04 +0300

    gitignore

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import json
+from datetime import datetime, time
+
+
+def greetings_time():
+    now = datetime.now()
+    current_time = now.time()
+    if current_time <= time(hour=12):
+        greeting = "Доброе утро"
+    elif current_time <= time(hour=18):
+        greeting = "Добрый день"
+    else:
+        greeting = "Добрый вечер"
+    response = {"greeting": greeting}
+    return json.dumps(response, ensure_ascii=False)
+
+print(greetings_time())
---
+++ src/views.py
@@ -0,0 +1,19 @@
+import json
+from datetime import datetime
+
+
+def date_time_to_json(date_time_str):
+    try:
+        date_time_obj = datetime.strptime(date_time_str, "%Y-%m-%d %H:%M:%S")
+        response = {
+            "date": date_time_obj.date().isoformat(),
+            "time": date_time_obj.time().isoformat(),
+        }
+    except ValueError:
+        response = {"status": "error", "message": "Неверный формат даты и времени"}
+    return json.dumps(response)
+
+
+date_time_input = "2023-10-05 14:30:00"
+json_output = date_time_to_json(date_time_input)
+print(json_output)
