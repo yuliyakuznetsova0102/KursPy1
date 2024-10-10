23dc80d4dcf7611115ddabbd483bdc5de6585711
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-10-10 23:33:34 +0300

    new function and test 3

--- src/report.py
+++ src/report.py
@@ -1,10 +1,21 @@
-import os
-from datetime import datetime, timedelta
+import logging
+from datetime import datetime
 
 import pandas as pd
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename="report.log",
+    filemode="w",
+)
+report_logger = logging.getLogger("report")
+
 
 def log_results_to_file(file_name):
+    """Декоратор для функций-отчетов, который записывает в файл результат,
+    который возвращает функция, формирующая отчет."""
+
     def decorator(func):
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
@@ -17,13 +28,14 @@ def log_results_to_file(file_name):
     return decorator
 
 
-@log_results_to_file("report_results.txt")
-def average_spending_per_weekday(transactions, date=None):
-    transactions["date"] = pd.to_datetime(transactions["date"])
+def expenses_by_category(transactions, category, date=None):
+    """Функция возвращает траты по заданной категории за последние три месяца (от переданной даты)."""
     if date is None:
         date = datetime.now()
-    three_months_ago = date - pd.DateOffset(months=3)
-    recent_transactions = transactions[transactions["date"] >= three_months_ago]
-    recent_transactions["weekday"] = recent_transactions["date"].dt.day_name()
-    average_spending = recent_transactions.groupby("weekday")["amount"].mean()
-    return average_spending.to_dict()
+        three_months_ago = date - pd.DateOffset(months=3)
+        filtered_transactions = transactions[
+            (transactions["category"] == category) & (transactions["date"] >= three_months_ago)
+        ]
+        total_expenses = filtered_transactions["amount"].sum()
+        report_logger.info("Вывод трат по заданной категории за последние три месяца")
+        return total_expenses
--- src/views.py
+++ src/views.py
@@ -1,37 +1,64 @@
 import json
-import pandas as pd
 from datetime import datetime
-import numpy
 
-def date_time_to_json(date_time_str):
-    try:
-        date_time_obj = datetime.strptime(date_time_str, "%Y-%m-%d %H:%M:%S")
-        response = {
-            "date": date_time_obj.date().isoformat(),
-            "time": date_time_obj.time().isoformat(),
-        }
-    except ValueError:
-        response = {"status": "error", "message": "Неверный формат даты и времени"}
-    return json.dumps(response)
+import pandas as pd
+
+from src.utils import get_card_data, get_currency_rates, get_sp500_stock_prices, get_top_transactions, greetings_time
 
 
+def read_excel_to_dict(file_path):
+    df = pd.read_excel(file_path)
+    records = df.to_dict(orient="records")
+    return records
 
 
+# Пример использования
+file_path = "../data/operations.xlsx"
+data = read_excel_to_dict(file_path)
+print(data)
 
-def get_excel(formatting):
-    current_transactions = []
-    get_excel_file = pd.read_excel("../data/operations.xlsx")
-    if formatting == "dataframe":
-        return get_excel_file
-    elif formatting == "dict":
-        for transaction in get_excel_file.to_dict(orient="records"):
-            transaction = {
-                key: (
-                    None if isinstance(value, float) and numpy.isnan(value) else value
-                )
-                for key, value in transaction.items()
-            }
-            current_transactions.append(transaction)
-        return current_transactions
-    else:
-        raise ValueError("Invalid format specified. Use 'dataframe' or 'dict'.")
+
+# Пример транзакций
+transactions = [
+    {"card_number": "1234567812345678", "amount": 250.0},
+    {"card_number": "1234567812345678", "amount": 150.0},
+    {"card_number": "8765432187654321", "amount": 300.0},
+    {"card_number": "8765432187654321", "amount": 50.0},
+    {"card_number": "1111222233334444", "amount": 100.0},
+    {"card_number": "1111222233334444", "amount": 200.0},
+]
+
+
+cards_data = [
+    {
+        "card_number": "1234567812345678",
+        "expenses": 1200,
+        "transactions": [
+            {"amount": 500, "description": "Покупка в магазине"},
+            {"amount": 700, "description": "Оплата за услуги"},
+            {"amount": 200, "description": "Перевод другу"},
+            {"amount": 100, "description": "Кофе"},
+            {"amount": 150, "description": "Обед"},
+            {"amount": 50, "description": "Книги"},
+        ],
+    }
+]
+
+
+def main(date_time_str):
+    current_datetime = datetime.strptime(date_time_str, "%Y-%m-%d %H:%M:%S")
+    response = {
+        "greetings_time": greetings_time(current_datetime),
+        "cards": get_card_data(cards_data),
+        "top_transactions": [get_top_transactions(card) for card in cards_data],
+        "currency_rates": get_currency_rates(),
+        "sp500_stock_prices": get_sp500_stock_prices(),
+    }
+
+    return json.dumps(response, ensure_ascii=False, indent=4)
+
+
+# Пример использования
+date_time_input = "2023-10-01 14:30:00"
+json_response = main(date_time_input)
+print(json_response)
